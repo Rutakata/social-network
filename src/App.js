4f8b2ac51515ed6ee6e1fc6fc3beb2197d49cb39
Rutakata <vladpelishenko14@gmail.com>
2021-11-14 19:08:51 +0200

    1111

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route} from "react-router-dom";
+import SideBar from "./Components/Sidebar/Sidebar";
+import UsersContainer from "./Components/Users/UsersContainer";
+import ChatsContainer from "./Components/Chats/ChatsContainer";
+import ProfileContainer from "./Components/Profile/ProfileContainer";
+import HeaderContainer from "./Components/Header/HeaderContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <div className="app">
+            <HeaderContainer/>
+            <SideBar />
+            <div className="appContent">
+                <Route path="/profile/:userId?" render={() => <ProfileContainer />}/>
+                <Route path="/messages" render={() => <ChatsContainer />}/>
+                <Route path="/users" render={() => <UsersContainer />}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;

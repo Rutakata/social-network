de13bb3185845926cbcadbaf19004d2d553aafef
Rutakata <vladpelishenko14@gmail.com>
2021-12-16 20:08:36 +0200

    Replaced class component ProfileStatus with functional component ProfileStatusWithHooks that uses hooks instead

--- src/Components/Profile/ProfileInfo/ProfileInfo.js
+++ src/Components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,8 +1,8 @@
 import React from "react";
 import style from "../Profile.module.css";
-import Preloader from "../../../Common/Preloader/preloader";
 import userPhoto from "../../../Assets/Images/user.jpg"
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 let ProfileInfo = (props) => {
     return (
@@ -10,7 +10,7 @@ let ProfileInfo = (props) => {
             <h1>{props.profileInfo.fullName}</h1>
             <img src={props.profileInfo.photos.large ? props.profileInfo.photos.large: userPhoto} className={style.avatar}/>
             <p>{!props.profileInfo.aboutMe ? "i am empty" : props.profileInfo.aboutMe}</p>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     )
 }
---
+++ src/Components/Profile/ProfileInfo/ProfileStatusWithHooks.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from "react";
+import style from "./ProfileStatus.module.css";
+
+
+const ProfileStatusWithHooks = (props) => {
+    let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
+
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <div className={style.status}>
+            {!editMode &&
+            <div>
+                <span onClick={activateEditMode}> {props.status || "No status"} </span>
+            </div>
+            }
+            {editMode &&
+            <div className={style.statusEdit}>
+                <input onChange={onStatusChange} value={status} /><br/>
+                <button onClick={deactivateEditMode}>Змінити</button>
+            </div>
+            }
+
+        </div>
+    )
+}
+
+export default ProfileStatusWithHooks
